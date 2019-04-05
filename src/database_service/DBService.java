69f79ec021577adc4669f49a3bc99c7802299b35
JULIAN MARIN <JCM5814@PSU-Erie.BD.Psu.Edu>
2019-04-05 18:42:50 -0400

    feat: Implement fetchVehicle and fetchHotel

--- src/database_service/DBService.java
+++ src/database_service/DBService.java
@@ -72,31 +72,8 @@ public class DBService {
             }
             
             /*
-            //construct sql request for hotel data
-            String hotelSQL = "SELECT * FROM HOTEL_ROOMS WHERE CITY = '" + cityTo + "' AND MAXGUESTS > " + guests + " ORDER BY PRICE";
             
             
-            //execute query
-            rs = stmt.executeQuery(hotelSQL);
-            
-            
-            //process data from hotel record
-            while(rs.next()){
-                FO = new HotelOption(rs.getInt("AVAILABILITY"), rs.getString("HOTELNAME"), rs.getInt("MAXGUESTS"), rs.getString("CITY"), rs.getInt("PRICE"));
-                optionList.add(FO);
-            }
-            
-            //construct sql request for vehicle data
-            String vehicleSQL = "SELECT * FROM VEHICLE_RENTALS WHERE CITY = '" + cityTo + "' AND MAXPASSENGERS > " + guests + " ORDER BY PRICE";
-            
-            //execute query
-            rs = stmt.executeQuery(vehicleSQL);
-            
-            //process data from hotel record
-            while(rs.next()){
-                FO = new VehicleOption(rs.getInt("AVAILABILITY"), rs.getString("CARMAKE"), rs.getString("CARMODEL"), rs.getInt("MAXPASSENGERS"), rs.getString("CITY"), rs.getInt("PRICE"));
-                optionList.add(FO);
-            }
             */
             
         } catch (SQLException err) {
@@ -105,13 +82,82 @@ public class DBService {
         return optionList;
     }
     
+    public List<HotelOption> fetchHotelData(String cityTo, int guests){
+        List<HotelOption> optionList = new ArrayList<HotelOption>();
+        
+        try {
+            ResultSet rs;
+            HotelOption HO;
+            Statement stmt = con.createStatement();
+            //construct sql request for hotel data
+            String hotelSQL = "SELECT * FROM HOTEL_ROOMS WHERE CITY = '" + cityTo + "' AND MAXGUESTS > " + guests + " ORDER BY PRICE";
+
+
+            //execute query
+            rs = stmt.executeQuery(hotelSQL);
+
+
+            //process data from hotel record
+            while(rs.next()){
+                HO = new HotelOption(rs.getInt("AVAILABILITY"), rs.getString("HOTELNAME"), rs.getInt("MAXGUESTS"), rs.getString("CITY"), rs.getInt("PRICE"));
+                optionList.add(HO);
+            }
+        }
+        catch(SQLException err){
+            
+        }
+        
+        return optionList;
+    }
+    
+    public List<VehicleOption> fetchVehicleData(String cityTo, int guests){
+        List<VehicleOption> optionList = new ArrayList<VehicleOption>();
+        
+        try {
+            ResultSet rs;
+            VehicleOption VO;
+            Statement stmt = con.createStatement();
+            //construct sql request for vehicle data
+            String vehicleSQL = "SELECT * FROM VEHICLE_RENTALS WHERE CITY = '" + cityTo + "' AND MAXPASSENGERS > " + guests + " ORDER BY PRICE";
+            
+            //execute query
+            rs = stmt.executeQuery(vehicleSQL);
+            
+            //process data from hotel record
+            while(rs.next()){
+                VO = new VehicleOption(rs.getInt("AVAILABILITY"), rs.getString("CARMAKE"), rs.getString("CARMODEL"), rs.getInt("MAXPASSENGERS"), rs.getString("CITY"), rs.getInt("PRICE"));
+                optionList.add(VO);
+            }
+        }
+        catch(SQLException err){
+            
+        }
+        
+        return optionList;
+    }
     
     public static void main(String[] args){
         DBService db = new DBService();
-        List<FlightOption> optionList = db.fetchFlightData("Pittsburgh", "Erie", 3);
-        for(FlightOption item : optionList){
-            System.out.println(item.getCityFrom());
-            System.out.println(item.getCityTo());
+        
+        //Get Flights
+        List<FlightOption> flightList = db.fetchFlightData("Pittsburgh", "Erie", 3);
+        System.out.println("Flights:");
+        for(FlightOption item : flightList){
+            System.out.println(item.getCityFrom() + " " + item.getCityTo() + " " + item.getPrice());
+        }
+        
+        //Get Hotels
+        List<HotelOption> hotelList = db.fetchHotelData("Erie", 3);
+        System.out.println("Hotels:");
+        for(HotelOption item : hotelList){
+            System.out.println(item.getName());
+        }
+        
+        //Get Vehicles
+        List<VehicleOption> vehicleList = db.fetchVehicleData("Erie", 3);
+        System.out.println("Vehicles:");
+        for(VehicleOption item : vehicleList){
+            System.out.println(item.getMake() + " " + item.getModel());
         }
     }
 }
