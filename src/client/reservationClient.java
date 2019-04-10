4d7b72190bd252df23a6c60931d4d924222d961d
Joshua Suttor <joshuasuttor@gmail.com>
2019-04-10 14:12:41 -0400

    feat: format and display response

--- src/client/reservationClient.java
+++ src/client/reservationClient.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 //import services.database_service.FlightOption;
 //import services.database_service.AbstractOption;
 //import services.database_service.HotelOption;
@@ -22,19 +23,25 @@ import java.net.URL;
  */
 public class reservationClient {
     
-    private static final String url = "http://localhost:8080/vehicle_service/VServ";
-    //private static final String url = "http://localhost:8080/hotel_service/HServ";
-    //private static final String url = "http://localhost:8080/flight_service/FServ";
+    private static final String vurl = "http://localhost:8080/vehicle_service/VServ";
+    private static final String hurl = "http://localhost:8080/hotel_service/HServ";
+    private static final String furl = "http://localhost:8080/flight_service/FServ";
+    private static final String cityTo = "Erie";
+    private static final String cityFrom = "Pittsburgh";
+    private static final int guests = 3;
+    
+    String url;
     
     /**
      * Creates new form CalculatorClient
      */
+   
     
     private void sendRequest(String cityTo, String cityFrom, int guests){
         try {
             HttpURLConnection conn = null;
             String requestURL = url + "?cityTo=" + cityTo + "&cityFrom=" + cityFrom + "&guests=" + guests;
-            System.out.println(requestURL);
+            //System.out.println(requestURL);
             conn = get_connection(requestURL, "GET");
             conn.addRequestProperty("accept", "text/plain");
             conn.connect();
@@ -44,9 +51,112 @@ public class reservationClient {
         catch(NullPointerException e) { System.err.println(e); }
     }
 
+    private void printResp(String resp){
+        int availability = 0;
+        int time = 0;
+        int price = 0;
+        String cityTo = "";
+        String cityFrom = "";
+        int guests = 0;
+        String name = "";
+        String make = "";
+        String model = "";
+        
+        int flightNum = 0;
+        int hotelNum = 0;
+        int vehicleNum = 0;
+               
+        String[] objs = resp.split("\\|");
+        for(String obj : objs){
+            String[] fields = obj.split(",");
+            if(fields[0].equals("flight")){
+                if(flightNum == 0){
+                    System.out.println("\nFlights:");
+                }
+                flightNum++;
+                for(int i = 1; i < fields.length; i++){
+                    String[] value = fields[i].split(":");
+                    if(i == 1){
+                        availability = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 2){
+                        time = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 3){
+                        price = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 4){
+                        cityTo = value[1];
+                    }
+                    else if(i == 5){
+                        cityFrom = value[1];
+                    }
+                }
+                if(cityTo.equals(this.cityTo)){
+                    System.out.println("  Flight to:");
+                }
+                else{
+                    System.out.println("  Return Flight:");
+                }
+                System.out.println("    Option " + flightNum + " From " + cityFrom + " to " + cityTo + " at " + time + "  Availability: " + availability + " seats");
+            }
+            else if(fields[0].equals("hotel")){
+                if(hotelNum == 0){
+                    System.out.println("\nHotels:");
+                }
+                hotelNum++;
+                for(int i = 1; i < fields.length; i++){
+                    String[] value = fields[i].split(":");
+                    if(i == 1){
+                        availability = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 2){
+                        name = value[1];
+                    }
+                    else if(i == 3){
+                        guests = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 4){
+                        price = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 5){
+                        cityTo = value[1];
+                    }
+                }
+                System.out.println("    Option " + hotelNum + " Hotel name: " + name + " in " + cityTo + " for up to " + guests + " guests.  Availability: " + availability + " rooms");
+            }
+            else{
+                if(vehicleNum == 0){
+                    System.out.println("\nVehicles:");
+                }
+                vehicleNum++;
+                for(int i = 1; i < fields.length; i++){
+                    String[] value = fields[i].split(":");
+                    if(i == 1){
+                        availability = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 2){
+                        make = value[1];
+                    }
+                    else if(i == 3){
+                        model = value[1];
+                    }
+                    else if(i == 4){
+                        guests = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 5){
+                        price = Integer.parseInt(value[1]);
+                    }
+                    else if(i == 6){
+                        cityTo = value[1];
+                    }
+                }
+                System.out.println("    Option " + vehicleNum + " Type: " + make + " " + model + " in " + cityTo + ".  Seats " + guests + " passenger.  Availability: " + availability + " seats");
+            }
+        } 
+    }
     
-    
-        private HttpURLConnection get_connection(String url_string, String verb) {
+    private HttpURLConnection get_connection(String url_string, String verb) {
         HttpURLConnection conn = null;
         try {
             URL url = new URL(url_string);
@@ -68,17 +178,21 @@ public class reservationClient {
             String next = null;
             while ((next = reader.readLine()) != null)
                 xml += next;
-            System.out.println(xml);
-            //String[] parts = xml.split(", ");
-            //for(String part : parts){
-               
-            //}
+            //System.out.println(xml);
+            printResp(xml);
+
         }
         catch(IOException e) { System.err.println(e); }
     }
     
         public static void main(String args[]) {
-            reservationClient rc = new reservationClient();
-            rc.sendRequest("Erie", "Pittsburgh", 3);
+            reservationClient rc = new reservationClient();       
+            
+            rc.url = vurl;
+            rc.sendRequest(cityTo, cityFrom, guests);
+            rc.url = hurl;
+            rc.sendRequest(cityTo, cityFrom, guests);
+            rc.url = furl;
+            rc.sendRequest(cityTo, cityFrom, guests);
         }
 }
